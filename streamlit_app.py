8e4d7204c173552ae66a6ee0e43c8c8dec2f461c
Ian Calvert <ianjcalvert@gmail.com>
2022-04-01 22:10:43 +0100

    Attempt fix caching

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,15 @@
 import streamlit as st
 import duckdb
+import pyarrow
 import pyarrow.dataset as ds
 
-@st.cache
-def get_connection():
 
-    # Gets Database Connection
-    return duckdb.connect()
-
-@st.cache
+@st.cache(hash_funcs={pyarrow.lib.Buffer: lambda _: None})
 def get_dataset():
     return ds.dataset('s3://ursa-labs-taxi-data/', partitioning=["year", "month"])
 
-
 nyc_dataset = get_dataset()
-con = get_connection()
+con = duckdb.connect()
 query = con.execute("SELECT * FROM nyc_dataset limit 10")
 
 st.write(query.fetchdf())
