259ee19c8e13fe1f6b3f0612a0105fe7899cd9b8
Ian Calvert <ianjcalvert@gmail.com>
2022-04-01 22:31:16 +0100

    setup scaffolding for timed queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
+from socket import create_connection
 import streamlit as st
 import duckdb
 from urllib.request import urlretrieve
 import pyarrow.dataset as ds
-
+from time import time
 
 @st.cache
 def download_dataset():
@@ -11,11 +12,21 @@ def download_dataset():
     urlretrieve(url, dst)
     return dst
 
+def run_timed_query(title, desc, query, connection):
+    st.header(title)
+    st.write(desc)
+    st.code(query)
+    start = time()
+    results = connection.execute(query).fetchdf()
+    end = time()
+    st.write(f'Query took {end - start} seconds')
+    st.write(results)
 
+# First we'll download the dataset
 download_dataset()
-con = duckdb.connect()
-query = con.execute("""SELECT sum(l_extendedprice * l_discount) AS revenue
-                FROM
-                'lineitemsf1.snappy.parquet';""")
 
-st.write(query.fetchdf())
+connection = duckdb.connect()
+
+run_timed_query("Simple aggregation", "This query uses projection pushdown",
+"""SELECT sum(l_extendedprice * l_discount) AS revenue
+    FROM 'lineitemsf1.snappy.parquet';""")
