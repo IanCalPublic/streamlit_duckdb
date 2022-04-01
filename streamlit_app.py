277c6f94697f60fef5966aea3cfa20bbfa12ef62
Ian Calvert <ianjcalvert@gmail.com>
2022-04-01 22:05:08 +0100

    Add basic query

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import duckdb
+import pyarrow.dataset as ds
 
-st.write("Hello world")
+@st.cache
+def get_connection():
+
+    # Gets Database Connection
+    return duckdb.connect()
+
+@st.cache
+def get_dataset():
+    return ds.dataset('s3://ursa-labs-taxi-data/nyc-taxi/', partitioning=["year", "month"])
+
+
+nyc_dataset = get_dataset()
+con = get_connection()
+query = con.execute("SELECT * FROM nyc_dataset limit 10")
+
+st.write(query.fetchdf())
